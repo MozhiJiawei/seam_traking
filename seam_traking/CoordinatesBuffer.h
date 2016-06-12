ba28e1fcec9148373fbb397a1506d0945a4a6cff
Mozhi Jiawei <qetwe0000@gmail.com>
2016-06-12 11:28:11 +0800

    CoordinatesBuffer. Last Edit before encrypt.

---
+++ seam_traking/CoordinatesBuffer.h
@@ -0,0 +1,13 @@
+#ifndef _COORDINATESBUFFER_H__
+#define _COORDINATESBUFFER_H__
+
+#include <opencv2/opencv.hpp>
+#include <queue>
+#include "RobotPose.h"
+class CoordinatesData {
+
+};
+
+class CoordinatesBuffer {
+};
+#endif /*_COORDINATESBUFFER_H__*/
